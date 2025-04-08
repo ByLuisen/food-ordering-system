fa3776e131c48b575b38d8004fd6bcecd6e65f56
ByLuisen <luisenric32@gmail.com>
2025-04-08 10:43:20 +0200

    Added avro schema files and startup script

--- restaurant-service/restaurant-container/src/main/java/com/food/ordering/system/restaurant/service/domain/RestaurantServiceApplication.java
+++ restaurant-service/restaurant-container/src/main/java/com/food/ordering/system/restaurant/service/domain/RestaurantServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.ProblemDetail;
 
 @EnableJpaRepositories(basePackages = {"com.food.ordering.system.restaurant.service.dataaccess", "com.food.ordering.system.dataaccess"})
 @EntityScan(basePackages = {"com.food.ordering.system.restaurant.service.dataaccess", "com.food.ordering.system.dataaccess"})
