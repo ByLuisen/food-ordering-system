d6bfbd2afe2de0d558adc6f339d8af8aee65b583
ByLuisen <luisenric32@gmail.com>
2025-04-14 18:18:31 +0200

    Implemented optimistic locking for credit entry

--- infrastructure/docker-compose/start-up.sh
+++ infrastructure/docker-compose/start-up.sh
@@ -55,96 +55,88 @@ done
 
 echo "Creating debezium connectors"
 
-curl --location --request POST 'localhost:8083/connectors' \
+curl --location 'http://localhost:8083/connectors' \
 --header 'Content-Type: application/json' \
---data-raw '{
+--data '{
   "name": "order-payment-connector",
   "config": {
-      "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
-      "tasks.max": "1",
-      "database.hostname": "host.docker.internal",
-      "database.port": "5432",
-      "database.user": "postgres",
-      "database.password": "admin",
-      "database.dbname" : "postgres",
-      "database.server.name": "PostgreSQL-15",
-      "table.include.list": "order.payment_outbox",
-      "topic.prefix": "debezium",
-      "tombstones.on.delete" : "false",
-      "slot.name" : "order_payment_outbox_slot",
-      "plugin.name": "pgoutput",
-      "auto.create.topics.enable": false,
-      "auto.register.schemas": false
-      }
- }'
+    "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+    "tasks.max": "1",
+    "database.hostname": "food-ordering-system-db",
+    "database.port": "5432",
+    "database.user": "postgres",
+    "database.password": "admin",
+    "database.dbname" : "postgres",
+    "database.server.name": "PostgreSQL-15",
+    "table.include.list": "order.payment_outbox",
+    "topic.prefix": "debezium",
+    "tombstones.on.delete" : "false",
+    "slot.name" : "order_payment_outbox_slot",
+    "plugin.name": "pgoutput"
+  }
+}'
 
-curl --location --request POST 'localhost:8083/connectors' \
+curl --location 'http://localhost:8083/connectors' \
 --header 'Content-Type: application/json' \
---data-raw '{
+--data '{
   "name": "order-restaurant-connector",
   "config": {
-      "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
-      "tasks.max": "1",
-      "database.hostname": "host.docker.internal",
-      "database.port": "5432",
-      "database.user": "postgres",
-      "database.password": "admin",
-      "database.dbname" : "postgres",
-      "database.server.name": "PostgreSQL-15",
-      "table.include.list": "order.restaurant_approval_outbox",
-      "topic.prefix": "debezium",
-      "tombstones.on.delete" : "false",
-      "slot.name" : "order_restaurant_approval_outbox_slot",
-      "plugin.name": "pgoutput",
-      "auto.create.topics.enable": false,
-      "auto.register.schemas": false
-      }
- }'
+    "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+    "tasks.max": "1",
+    "database.hostname": "food-ordering-system-db",
+    "database.port": "5432",
+    "database.user": "postgres",
+    "database.password": "admin",
+    "database.dbname" : "postgres",
+    "database.server.name": "PostgreSQL-15",
+    "table.include.list": "order.restaurant_approval_outbox",
+    "topic.prefix": "debezium",
+    "tombstones.on.delete" : "false",
+    "slot.name" : "order_restaurant_approval_outbox_slot",
+    "plugin.name": "pgoutput"
+  }
+}'
 
-curl --location --request POST 'localhost:8083/connectors' \
+curl --location 'http://localhost:8083/connectors' \
 --header 'Content-Type: application/json' \
---data-raw '{
+--data '{
   "name": "payment-order-connector",
   "config": {
-      "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
-      "tasks.max": "1",
-      "database.hostname": "host.docker.internal",
-      "database.port": "5432",
-      "database.user": "postgres",
-      "database.password": "admin",
-      "database.dbname" : "postgres",
-      "database.server.name": "PostgreSQL-15",
-      "table.include.list": "payment.order_outbox",
-      "topic.prefix": "debezium",
-      "tombstones.on.delete" : "false",
-      "slot.name" : "payment_order_outbox_slot",
-      "plugin.name": "pgoutput",
-      "auto.create.topics.enable": false,
-      "auto.register.schemas": false
-      }
- }'
+    "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+    "tasks.max": "1",
+    "database.hostname": "food-ordering-system-db",
+    "database.port": "5432",
+    "database.user": "postgres",
+    "database.password": "admin",
+    "database.dbname" : "postgres",
+    "database.server.name": "PostgreSQL-15",
+    "table.include.list": "payment.order_outbox",
+    "topic.prefix": "debezium",
+    "tombstones.on.delete" : "false",
+    "slot.name" : "payment_order_outbox_slot",
+    "plugin.name": "pgoutput"
+  }
+}'
 
-curl --location --request POST 'localhost:8083/connectors' \
+curl --location 'http://localhost:8083/connectors' \
 --header 'Content-Type: application/json' \
---data-raw '{
+--data '{
   "name": "restaurant-order-connector",
   "config": {
-      "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
-      "tasks.max": "1",
-      "database.hostname": "host.docker.internal",
-      "database.port": "5432",
-      "database.user": "postgres",
-      "database.password": "admin",
-      "database.dbname" : "postgres",
-      "database.server.name": "PostgreSQL-15",
-      "table.include.list": "restaurant.order_outbox",
-      "topic.prefix": "debezium",
-      "tombstones.on.delete" : "false",
-      "slot.name" : "restaurant_order_outbox_slot",
-      "plugin.name": "pgoutput",
-      "auto.create.topics.enable": false,
-      "auto.register.schemas": false
-      }
- }'
+    "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+    "tasks.max": "1",
+    "database.hostname": "food-ordering-system-db",
+    "database.port": "5432",
+    "database.user": "postgres",
+    "database.password": "admin",
+    "database.dbname" : "postgres",
+    "database.server.name": "PostgreSQL-15",
+    "table.include.list": "restaurant.order_outbox",
+    "topic.prefix": "debezium",
+    "tombstones.on.delete" : "false",
+    "slot.name" : "restaurant_order_outbox_slot",
+    "plugin.name": "pgoutput"
+  }
+}'
 
 echo "Start-up completed"
