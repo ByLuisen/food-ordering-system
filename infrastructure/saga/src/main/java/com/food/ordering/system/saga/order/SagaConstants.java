c8fdee52377afa55ab217cd45562ef72c1954b3d
ByLuisen <luisenric32@gmail.com>
2025-04-21 14:20:56 +0200

    refactoring

--- infrastructure/saga/src/main/java/com/food/ordering/system/saga/order/SagaConstants.java
+++ infrastructure/saga/src/main/java/com/food/ordering/system/saga/order/SagaConstants.java
@@ -3,7 +3,6 @@ package com.food.ordering.system.saga.order;
 public final class SagaConstants {
 
     private SagaConstants() {
-
     }
 
     public final static String ORDER_SAGA_NAME = "OrderProcessingSaga";
